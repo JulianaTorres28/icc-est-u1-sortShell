6110b025bf554204990f02554597c552dfbb1398
Juliana Torres <julianatc21@gmail.com>
2025-10-29 21:39:46 -0500

    Acciones-Método Shell

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] numeros = {5, 0, 8, 4, 9 , 6, 7, 2, 3};
+        View view = new View();
+        SortShell sortShell = new SortShell();
+
+        System.out.println("Arreglo Original: ");
+        view.printArray(numeros);
+        sortShell.sort(numeros, false);
+        System.out.println("Ordenado: ");
+        view.printArray(numeros);
     }
 }
--- src/SortShell.java
+++ src/SortShell.java
@@ -1,3 +1,43 @@
 public class SortShell {
 
+    public SortShell(){
+        System.out.println("Metodo Shell");
+    }
+
+    public void sort(int[] numeros, boolean asc ){
+        int n = numeros.length;
+        int comparaciones = 0;
+        int cambios = 0;
+
+
+        for (int gap = n/2 ; gap > 0 ; gap /= 2) {
+            //Insercion
+            for (int i = gap; i < n; i++) {
+                int temp = numeros[i];
+                int j = i;
+                boolean swap = false;
+
+                while (j >= gap && numeros[j - gap] > temp) {
+                    comparaciones++;
+                    numeros[j] = numeros[j - gap];
+                    j -= gap;
+                    cambios++;
+                    swap = true;
+                }
+                
+
+                if (j >= gap) {
+                    comparaciones++;
+                }
+
+                if (swap) {
+                    numeros[j] = temp;
+                }
+            }
+        
+        }
+
+
+        System.out.println("Acciones(comparaciones+cambios)= " + (comparaciones+cambios));
+    }
 }
